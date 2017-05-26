e76e4de4bc826f5c2acf2bb41e9b5b28a9005eac
rampal singh <rampal.a.singh@gmail.com>
2017-05-26 20:07:46 +0530

    commit6

--- src/testCases/Demo.java
+++ src/testCases/Demo.java
@@ -1,7 +1,7 @@
 package testCases;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.Test;
 
 public class Demo {
@@ -9,8 +9,12 @@ public class Demo {
 	@Test
 	public void f(){
 		
-		System.setProperty("webdriver.chrome.driver", ".//chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
+//		System.setProperty("webdriver.chrome.driver", ".//chromedriver.exe");
+		WebDriver driver = new HtmlUnitDriver();
+	
+		driver.get("http://www.google.com");
+		 
+		// Print the title
+		System.out.println("Title of the page : "+ driver.getTitle());
 	}
 }
